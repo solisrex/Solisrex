ebd91fed2c504430ae649a03d5528c096b55b414
Alex Siryj <alexandersiryj@outlook.com>
2022-02-25 21:23:27 +1000

    Changed over to MySQL

--- index.js
+++ index.js
@@ -1,69 +1,41 @@
-const { MongoClient } = require("mongodb");
+var mysql      = require('mysql');
+
 const express = require('express')
 const app = express()
 const port = 8080
 
 app.set('view engine', 'pug')
 
-// Connection URI
-const uri =
-  "mongodb+srv://readonly:EzQqMZ4kyRhZVQ7@cluster0.xde3j.azure.mongodb.net/mathematics";
-
-// Create a new MongoClient
-const client = new MongoClient(uri);
 
 async function getCollections(req,res) {
-    try {
-        // Connect the client to the server
-        await client.connect();
-
-        // Establish and verify connection
-        const database = client.db('Math');
-        var x = await (await database.collections()).map(x=>x.collectionName)
-        res.render('index', {title : "Collections",categories : x})
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+    
 }
 
 async function getDocuments(collection, req, res) {
-    try {
-        // Connect the client to the server
-        await client.connect();
-
-        // Establish and verify connection
-        const database = client.db('Math');
-        const collectionObject = await database.collection(collection)
-        await collectionObject.find({},{sort : {title : 1}, projection : {_id: 0, title: 1, content: 1}}).toArray().then(
-            result => res.render('categories', {title : collection, documents: result.map(x => x.title)})
-        )
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+    
 }
 
 async function renderDocument(collection, document, req, res) {
-    try {
-        // Connect the client to the server
-        await client.connect();
-
-        // Establish and verify connection
-        const database = client.db('Math');
-        const collectionObject = await database.collection(collection)
-        await collectionObject.findOne({title : document}).then(
-            result => res.render('document', {title : document, collection: collection, content : result.content})
-        )
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+    
 }
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+    var connection = mysql.createConnection({
+        host     : 'db-mysql-sgp1-44809-do-user-10968463-0.b.db.ondigitalocean.com',
+        user     : 'doadmin',
+        password : '8zNuD8Zs7KAgvrgC',
+        database : 'mathematics'
+      });
+    
+      connection.connect();
+     
+      connection.query('SELECT * FROM propositions', function (error, results, fields) {
+        if (error) throw error;
+        res.send("success")
+      });
+      connection.end();
 })
 
 app.get('/main', (req, res) => {
